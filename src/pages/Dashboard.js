250d710e7df46d3c41d13393f66ff15997bc90c8
Parminder Klair <perminder.klair@gmail.com>
2016-12-09 11:11:20 +0000

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -29,7 +29,18 @@ class Dashboard extends Component {
                     Dashboard
                 </h3>
                 <div className="ui attached segment">
-                    <p>Welcome to tikkie.</p>
+                     <p>Welcome to react starting poing.</p>
+                     <div className="ui divider" />
+ 
+                     <div className="ui medium header">Gallery</div>
+                     <div className="ui small images">
+                         {gallery.data.length === 0 ?
+                             <p>No gallery images found</p>
+                             :
+                                 gallery.data.map(item =>
+                                     <img key={item._id} src={item.data.image.url} alt={item.data.title} />
+                         )}
+                     </div>
                 </div>
             </div>
         );
